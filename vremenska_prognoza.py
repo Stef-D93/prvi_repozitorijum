6a1a13409c940146f4de376d2e509dd52a45e427
Stefan Dendić <stefandendic@yahoo.com>
2025-03-01 10:22:53 +0100

    Dodat test

--- vremenska_prognoza.py
+++ vremenska_prognoza.py
@@ -1,7 +1,16 @@
 temperatura = int(input("Unesite temperaturu: "))
+test_temperatura = -1
+temperatura = test_temperatura
+poruka = ""
 if temperatura <0:
-    print("Oprez, klizavo!")
+    poruka = "Oprez, klizavo!"
 if temperatura > 0:
-    print("Temperatura iznad 0")
+    poruka = "Temperatura iznad 0"
     if temperatura >30:
-        print("Ukljucite klima uredjaje")
+        poruka = "Ukljucite klima uredjaje"
+
+#testiranje iznad navedenog koda - case <0 
+# NAPOMENA: test se piše izvan ovog fajla. Ovo je čisto za primjer.
+ocekivana_poruka = "Oprez, klizavo!"
+if ocekivana_poruka == poruka:
+    print("Case ispod nule - test prošao")
